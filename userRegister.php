e1a0eab81ed2401e3ff47457df44f3d1b759424d
allengawlowicz <agawlowicz@email.neit.edu>
2022-08-17 09:47:22 -0400

    s

---
+++ userRegister.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Register</title>
+    <link rel="stylesheet" type="text/css" href="style.css" >
+</head>
+<body>
+<div class="container">
+<div class= "header">
+</div>
+
+<div class="info">
+    <h4>Registration</h4>
+<form id="userRegister" method="post" action="userRegister.php">
+<div class="main">
+    <div class="label">
+    <label>First Name:</label>
+    </div>
+    <div>
+    <input type="text" name="fName" value="<?php $fName;?>" ><br>
+    </div>
+<br>
+    <div class="label">
+    <label>Email Address:</label>
+    </div>
+    <div>
+    <input type="text" name="email" value="<?php $email;?>" >
+    </div>
+<br>
+    <div class="label">
+    <label>Password:</label>
+    </div>
+    <div>
+    <input type="text" name="password" value="<?php $password;?>" >
+    </div>
+<br>
+<div class="label">
+    <label>Confirm Password:</label>
+    </div>
+    <div>
+    <input type="text" name="confirmPassword" value="<?php $confirmPassword;?>" >
+    </div>
+<br>
+</div>
+<form id="print" method="POST" action="userLogin.php">
+    <input type="submit" name="final" value="Register"/>
+</form>
+<p>Already have an account? <a href='userLogin.php'>Login Here</a></p>
+
+
+</form>
+</div>
+
+</div>
+</body>
+</html>
+
+<?php 
+
+
+    function validation($fName, $email, $password,$confirmPassword){
+        if($fName =="" || $email =="" || $password =="" || $confirmPassword ==""){
+            $fName= "Please enter a first name!";
+            $email= "Invalid Email Address!";
+            $password= "Please enter a password!";
+            $confirmPassword= "Please re-enter your password!";
+
+
+        }
+
+    };
+
+    $fName = filter_input(INPUT_POST, 'fName');
+    $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+    $password = filter_input(INPUT_POST, 'password');
+    $confirmPassword = filter_input(INPUT_POST, 'confirmPassword');
+
+
+    
+
+
+
+    
+
+  
+
+
+
+    if(isset($_POST['final'])){
+       echo "Submitted <hr />";
+       echo $fName;
+       echo "<hr />";
+       echo $email;
+       echo "<hr />";
+       echo $password;              
+       echo "<hr />";
+       echo $confirmPassword;
+        
+    };
